bea3fe267046b8190ff3b1c9425235b2b93f685d
Bit4Bit <bit4bit@riseup.net>
2012-09-09 19:24:30 -0500

    cliente prioridad por hangupcause

--- app/models/client.rb
+++ app/models/client.rb
@@ -12,4 +12,26 @@ class Client < ActiveRecord::Base
   #  :with => /[0-9]+([ ][0-9]+)*/,
   #  :message => "Solo número entre 7 y 10 digitos separados por espacio"
   #}
+
+  #Cambia prioridad del cliente segun la cause de cuelgue
+  #el objetivo primordial es darle prioridad en una proxima llamada
+  #a los clientes que contestaron y a los que no ir bajandole prioridad
+  #http://wiki.freeswitch.org/wiki/Hangup_causes
+  def update_priority_by_hangup_cause(cause)
+    case cause
+    when 'NORMAL_CLEARING'
+      priority += 100
+    when 'ALLOTED_TIMEOUT'
+      priority -= 100
+    when 'USER_BUSY'
+      priority -= 300
+    when 'CALL_REJECTED'
+      priority -= 900
+    when 'UNALLOCATED_NUMBER '
+      priority -= 1000
+    else
+      priority -= 500
+    end
+
+    self.save()
 end
