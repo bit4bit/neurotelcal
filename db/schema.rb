a8ce137be5324067a26b9875be462711b5735769
root <root@titan264.startdedicated.com>
2012-09-13 12:33:51 -0500

    mezclaw

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912035010) do
+ActiveRecord::Schema.define(:version => 20120911033644) do
 
   create_table "calendars", :force => true do |t|
     t.integer  "message_id"
@@ -143,7 +143,6 @@ ActiveRecord::Schema.define(:version => 20120912035010) do
     t.datetime "created_at",                                            :null => false
     t.datetime "updated_at",                                            :null => false
     t.string   "phonenumber",      :default => "0000000000"
-    t.boolean  "calling",          :default => false
   end
 
   create_table "resources", :force => true do |t|
