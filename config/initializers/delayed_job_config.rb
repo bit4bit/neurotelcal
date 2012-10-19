720fa20377825e86257d9c49422dfa2a8ac2db4d
Bit4Bit <bit4bit@riseup.net>
2012-10-19 18:06:29 -0500

    excepcution expire se aumenta tiempo..aunque esta no es la solucion :)

---
+++ config/initializers/delayed_job_config.rb
@@ -0,0 +1,19 @@
+# Copyright (C) 2012 Bit4Bit <bit4bit@riseup.net>
+#
+#
+# This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#Bug con la duracion del serivico activo
+Delayed::Worker.max_run_time = 99999.hours
