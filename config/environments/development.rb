d67e5d880f7070e2a4153b66865165247a2ff1be
Bit4Bit <bit4bit@riseup.net>
2013-02-27 19:52:29 -0500

    corr

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,6 @@ Neurotelcal::Application.configure do
 
   #correoe ivr
   config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 end
