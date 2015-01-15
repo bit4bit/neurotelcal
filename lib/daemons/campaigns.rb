27254e5deb37b8d68d362b5e1c3ea8a72f1197d9
Jovany Leandro G.C <bit4bit@riseup.net>
2015-01-15 18:20:31 -0500

    corr

--- lib/daemons/campaigns.rb
+++ lib/daemons/campaigns.rb
@@ -24,17 +24,28 @@ while($running) do
       $running_campaigns.delete(campaign.id) if campaign.end?
       next
     end
+    next unless campaign.start?
     Rails.logger.info "Testing campaign #{campaign.name}"
-    
-    Rails.logger.info "Forking process for campaign #{campaign.name}"
+
+
     ::ActiveRecord::Base.establish_connection
     pid = fork do
-      until campaign.end?
+      fork_campaign = Campaign.find(campaign.id)
+      Rails.logger.info "Forking process for campaign #{fork_campaign.name} ENV #{ENV['RAILS_ENV']}"
+      ENV["RAILS_ENV"] ||= "production"
+
+      root = File.expand_path(File.dirname(__FILE__))
+      root = File.dirname(root) until File.exists?(File.join(root, 'config'))
+      Dir.chdir(root)
+
+      require File.join(root, "config", "environment")
+
+      while fork_campaign.start?
         Rails.logger.debug("Processing campaign #{campaign.name}")
-        campaign.process(true)
+        fork_campaign.process(true)
         sleep 5
-        break unless $running
       end
+      Rails.logger.debug("Stopped campaign #{campaign.name}")
     end
    $running_campaigns[campaign.id] = pid
   end
@@ -42,6 +53,6 @@ while($running) do
   sleep 10
 end
 
-$running_campaigns.each do |campaign_id, pid|
-  Process.wait pid
+$running_campaigns.each do |campaign_id, task|
+  Process.waitpid pid
 end
